38989b277db4369cd64927934ada8263faf187fb
Han Hoang <katiehoang268@gmail.com>
2023-02-18 18:02:15 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// Basic React code is at the end of the page
+import { v4 as uuidv4 } from 'uuid'
+import { useState } from "react"
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from "./data/FeadbackData"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete')){
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+  return (//return only one parent element
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        
+      </div>
+    </>
+          
+  )
 }
 
-export default App;
+export default App
+/*
+function App(){
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    {id: 1, text: 'Comment 1'},
+    {id: 2, text: 'Comment 2'},
+    {id: 3, text: 'Comment 3'}
+  ]
+  const loading = false
+  const showComments = true
+
+  if (loading) return <h1>Loading...</h1>
+
+  const commentBlock = (
+    <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={comment.index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+  )
+  return (//return only one parent element
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
+
+    </div>      
+  )
+}
+*/
