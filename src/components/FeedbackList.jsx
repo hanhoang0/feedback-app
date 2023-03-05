3011ab85f0afc326d130bb2ec776d657bb292c54
Han Hoang <katiehoang268@gmail.com>
2023-03-04 21:06:11 -0600

    update server

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,21 @@
 import FeedbackItem from './FeedbackItem'
 import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
+import spinner from './assets/spinner.gif'
 
 function FeedbackList() {
-  const {feedback} = useContext(FeedbackContext)
+  const {feedback, isLoading} = useContext(FeedbackContext)
 
-  if (feedback.length === 0){
+  if (!isLoading && feedback.length === 0){
     return <p>No Feedback Yet</p>
   }
-  return (
+
+  return isLoading ? 
+  <img
+    src={spinner}
+    alt='Loading...'
+    style={{width: '100px', margin: 'auto', display:'block'}}
+  /> : (
     <div className='feedback-list'>
       {feedback.map((item) => (
         <FeedbackItem 
@@ -18,6 +25,7 @@ function FeedbackList() {
       ))}
     </div>
   )
+      
 }
 
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ function Header({ text, bgColor, textColor }) {
 }
 
 Header.defaultProps = {
-  text: "Feedback UI",
+  text: "LoveFeedback",
   bgColor: "rgba(0,0,0,0.4)",
   textColor: "#ff6a95"
 }
